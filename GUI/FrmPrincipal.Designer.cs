7a739cf597918864aef1c7d74e3883db2d679f07
VerdugoCarlosFabian <89544727+VerdugoCarlosFabian@users.noreply.github.com>
2021-10-16 20:01:08 -0300

    Creacion Forms

--- GUI/FrmPrincipal.Designer.cs
+++ GUI/FrmPrincipal.Designer.cs
@@ -29,131 +29,19 @@ namespace Banco.GUI
         /// </summary>
         private void InitializeComponent()
         {
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.soporteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.transaccionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.extractoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.desarrolladoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.nuevaTransferenciaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.consultarSaldoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.consultarCBUToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.archivoToolStripMenuItem,
-            this.soporteToolStripMenuItem,
-            this.transaccionToolStripMenuItem,
-            this.ayudaToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(587, 24);
-            this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // archivoToolStripMenuItem
-            // 
-            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.salirToolStripMenuItem});
-            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
-            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
-            this.archivoToolStripMenuItem.Text = "Archivo";
-            // 
-            // salirToolStripMenuItem
-            // 
-            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
-            this.salirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.salirToolStripMenuItem.Text = "Salir";
-            // 
-            // soporteToolStripMenuItem
-            // 
-            this.soporteToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.nuevaTransferenciaToolStripMenuItem,
-            this.consultarSaldoToolStripMenuItem,
-            this.consultarCBUToolStripMenuItem});
-            this.soporteToolStripMenuItem.Name = "soporteToolStripMenuItem";
-            this.soporteToolStripMenuItem.Size = new System.Drawing.Size(85, 20);
-            this.soporteToolStripMenuItem.Text = "Operaciones";
-            // 
-            // transaccionToolStripMenuItem
-            // 
-            this.transaccionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.extractoToolStripMenuItem});
-            this.transaccionToolStripMenuItem.Name = "transaccionToolStripMenuItem";
-            this.transaccionToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
-            this.transaccionToolStripMenuItem.Text = "Reportes";
-            // 
-            // extractoToolStripMenuItem
-            // 
-            this.extractoToolStripMenuItem.Name = "extractoToolStripMenuItem";
-            this.extractoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.extractoToolStripMenuItem.Text = "Extracto";
-            // 
-            // ayudaToolStripMenuItem
-            // 
-            this.ayudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.desarrolladoresToolStripMenuItem});
-            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
-            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
-            this.ayudaToolStripMenuItem.Text = "Acerca de";
-            // 
-            // desarrolladoresToolStripMenuItem
-            // 
-            this.desarrolladoresToolStripMenuItem.Name = "desarrolladoresToolStripMenuItem";
-            this.desarrolladoresToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.desarrolladoresToolStripMenuItem.Text = "Desarrolladores";
-            // 
-            // nuevaTransferenciaToolStripMenuItem
-            // 
-            this.nuevaTransferenciaToolStripMenuItem.Name = "nuevaTransferenciaToolStripMenuItem";
-            this.nuevaTransferenciaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.nuevaTransferenciaToolStripMenuItem.Text = "Nueva transferencia";
-            // 
-            // consultarSaldoToolStripMenuItem
-            // 
-            this.consultarSaldoToolStripMenuItem.Name = "consultarSaldoToolStripMenuItem";
-            this.consultarSaldoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.consultarSaldoToolStripMenuItem.Text = "Consultar saldo";
-            // 
-            // consultarCBUToolStripMenuItem
-            // 
-            this.consultarCBUToolStripMenuItem.Name = "consultarCBUToolStripMenuItem";
-            this.consultarCBUToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.consultarCBUToolStripMenuItem.Text = "Consultar CBU";
             // 
             // FrmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(587, 389);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
             this.Name = "FrmPrincipal";
             this.Text = "Principal";
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem soporteToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem transaccionToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem extractoToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem desarrolladoresToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem nuevaTransferenciaToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem consultarSaldoToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem consultarCBUToolStripMenuItem;
     }
 }
